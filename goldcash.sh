e0ec7da4ad392161959952801b6fdb71b2df1a62
joezau <37699577+joezau@users.noreply.github.com>
2021-05-15 13:40:37 +0700

    Create goldcash.sh

---
+++ goldcash.sh
@@ -0,0 +1,5 @@
+#goldcash
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-asia.rplant.xyz:17057 -u Gfz1cxghzE3QoegWzTeSmpoe8ojz4bcJTy.netnot -p password=nadiul
